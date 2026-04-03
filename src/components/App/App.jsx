713f65e8a6a8d40f642aca2da7035c7de26e966d
joel <joelqui15@hotmail.com>
2026-04-02 16:34:56 -0800

    start interactivity

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,82 +6,89 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
 import { defaultClothingItems } from "../../utils/constants.js";
 
 function App() {
+  //state
   const [clothingItems, setClothingItems] = useState(defaultClothingItems);
-
   const [weatherData, setWeatherData] = useState({ type: "" });
-
+  const [activeModal, setAcvtiveModal] = useState("");
+  function openModal() {
+    setAcvtiveModal("add-garment");
+  }
   return (
     <>
       <div className="page">
         <div className="page__content">
           <Header weatherData={weatherData} />
           <Main clothingItems={clothingItems} weatherData={weatherData} />
-          <ModalWithForm>
-            <fieldset className="form__fieldset-info">
-              <label htmlFor="name" className="form__label form__label-name">
-                Name
-                <input
-                  id="name"
-                  type="text"
-                  name="name"
-                  className="form__input form__input-name"
-                  placeholder="Name"
-                />
-              </label>
-              <label htmlFor="url" className="form__label form__label-image">
-                Image
-                <input
-                  id="url"
-                  type="url"
-                  name="url"
-                  className="form__input form__input-name"
-                  placeholder="Image URL"
-                />
-              </label>
-            </fieldset>
-            <fieldset className="form__fieldset_radio">
-              <label
-                htmlFor="radio__hot"
-                className="form__label form__label-radio"
-              >
-                <input
-                  value="hot"
-                  id="radio__hot"
-                  type="radio"
-                  name="weather"
-                  className="form__input form__input-radio"
-                />
-                Hot
-              </label>
-              <label
-                htmlFor="radio__warm"
-                className="form__label form__label-radio"
-              >
-                <input
-                  value="warm"
-                  id="radio__warm"
-                  type="radio"
-                  name="weather"
-                  className="form__input form__input-radio"
-                />
-                Warm
-              </label>
-              <label
-                htmlFor="radio__cold"
-                className="form__label form__label-radio"
-              >
-                <input
-                  value="cold"
-                  id="radio__cold"
-                  type="radio"
-                  name="weather"
-                  className="form__input form__input-radio"
-                />
-                Cold
-              </label>
-            </fieldset>
-          </ModalWithForm>
         </div>
+        <ModalWithForm isOpen={activeModal === "add-garment"}>
+          <fieldset className=" form__fieldset form__fieldset-info">
+            <label htmlFor="name" className="form__label form__label-name">
+              Name
+              <br />
+              <input
+                id="name"
+                type="text"
+                name="name"
+                className="form__input form__input-name"
+                placeholder="Name"
+              />
+            </label>
+            <label htmlFor="url" className="form__label form__label-image">
+              Image
+              <br />
+              <input
+                id="url"
+                type="url"
+                name="url"
+                className="form__input form__input-name"
+                placeholder="Image URL"
+              />
+            </label>
+          </fieldset>
+          <fieldset className=" form__fieldset form__fieldset-radio">
+            <label
+              htmlFor="radio__hot"
+              className="form__label form__label-radio"
+            >
+              Select the weather type:
+              <br />
+              <input
+                value="hot"
+                id="radio__hot"
+                type="radio"
+                name="weather"
+                className="form__input form__input-radio"
+              />
+              Hot
+            </label>
+            <label
+              htmlFor="radio__warm"
+              className="form__label form__label-radio"
+            >
+              <input
+                value="warm"
+                id="radio__warm"
+                type="radio"
+                name="weather"
+                className="form__input form__input-radio"
+              />
+              Warm
+            </label>
+            <label
+              htmlFor="radio__cold"
+              className="form__label form__label-radio"
+            >
+              <input
+                value="cold"
+                id="radio__cold"
+                type="radio"
+                name="weather"
+                className="form__input form__input-radio"
+              />
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
       </div>
     </>
   );
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,15 +1,19 @@
-function ModalWithForm(props) {
+import "../ModalWithForm/ModalWithForm.css";
+
+function ModalWithForm({ children, isOpen }) {
   return (
     <>
-      <div className="modal">
-        <p className="modal__title">title</p>
-        <button type="button" className="modal__close-btn">
-          X
-        </button>
-        <form name="form" className="form">
-          {props.children}
+      <div className={isOpen ? "modal" : "modal__hidden"}>
+        <form name="form" className="modal__form">
+          <h2 className="modal__title">title</h2>
+          <button type="button" className="modal__close-btn">
+            X
+          </button>
+          {children}
+          <button type="submit" className="modal__submit-btn">
+            SUBMIT
+          </button>
         </form>
-        <button type="submit" className="modal__close-btn"></button>
       </div>
     </>
   );
