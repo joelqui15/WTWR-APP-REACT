f58309e81af86c2c3e9211341f586898359d1e70
joel <joelqui15@hotmail.com>
2026-04-29 15:30:33 -0800

    feat: delete modal complete and fully functional

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -2,53 +2,58 @@ import { useEffect } from "react";
 import "../DeleteModal/DeleteModal.css";
 
 function DeleteModal({ isOpen, onDelete, card, onClose }) {
-  function ItemModal({ onClose, isOpen, card, openModal }) {
-    useEffect(() => {
-      if (!isOpen) return;
-      function handleEscape(e) {
-        if (e.key === "Escape") {
-          onClose();
-        }
-      }
-      document.addEventListener("keydown", handleEscape);
-
-      return () => {
-        document.removeEventListener("keydown", handleEscape);
-      };
-    }, [isOpen, onClose]);
-
-    function handleCloseClick(e) {
-      if (e.target === e.currentTarget) {
+  useEffect(() => {
+    if (!isOpen) return;
+    function handleEscape(e) {
+      if (e.key === "Escape") {
         onClose();
       }
     }
+    document.addEventListener("keydown", handleEscape);
 
-    function handleDelete() {
-      onDelete(card._id);
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [isOpen, onClose]);
+
+  function handleCloseClick(e) {
+    if (e.target === e.currentTarget) {
+      onClose();
     }
+  }
 
-    return (
-      <div className={isOpen ? "modal" : "modal__hidden"}>
-        <div className="modal__delete">
-          <button type="button" className="modal__close-btn">
-            X
-          </button>
-          <p className="modal__delete-text">
-            Are you sure you want to delete this item?
-            <br />
-            This action is irreversible.
-          </p>
+  function handleDelete() {
+    onDelete(card._id);
+  }
+
+  return (
+    <div className={isOpen ? "modal" : "modal__delete-hidden"}>
+      <div className="modal__delete-content">
+        <button
+          type="button"
+          className="modal__close-btn_type_delete"
+          onClick={handleCloseClick}
+        ></button>
+        <p className="modal__delete-text">
+          Are you sure you want to delete this item?
+          <br />
+          This action is irreversible.
+        </p>
+        <div className="modal__delete-buttons">
           <button
             type="button"
-            className="modal__delete-btn"
+            className="modal__delete-btn_type_delete"
             onClick={handleDelete}
           >
             Yes, delete this item
           </button>
-          <button className="modal__cancel-btn">Cancel</button>
+          <button className="modal__cancel-btn" onClick={handleCloseClick}>
+            Cancel
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default DeleteModal;
